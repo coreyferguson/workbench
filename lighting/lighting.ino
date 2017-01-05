8edaa5ad2e3acd3878fb2002aa0189d1a660f410
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-01-05 10:05:14 -0800

    ... pir and light only ...

--- lighting/lighting.ino
+++ lighting/lighting.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_NeoPixel.h>
+
 #define BAUD_RATE 9600
 
 // Motion Sensor
@@ -6,10 +8,17 @@
 // Digital output
 #define PIN_PIR A2
 
-#define PIN_LED 13
+// Light
+#define PIN_LED 6
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN_LED, NEO_GRBW + NEO_KHZ800);
+uint32_t white = strip.Color(255, 255, 255);
+uint32_t black = strip.Color(0, 0, 0);
 
 void setup() {
   pinMode(PIN_PIR, INPUT);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 
   Serial.begin(BAUD_RATE);
   Serial.println("setup() complete");
@@ -17,12 +26,23 @@ void setup() {
 
 void loop() {
   static bool pirActive;
+  static bool pirWasActive;
+  pirWasActive = pirActive;
   pirActive = digitalRead(PIN_PIR);
+
   Serial.println(pirActive, DEC);
-  if (pirActive) {
-    digitalWrite(PIN_LED, HIGH);
-  } else {
-    digitalWrite(PIN_LED, LOW);
+
+  if (!pirWasActive && pirActive) {
+    for (int i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, white);
+    }
+    strip.show();
+  } else if (pirWasActive && !pirActive) {
+    for (int i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, black);
+    }
+    strip.show();
   }
-  delay(500);
+
+  delay(50);
 }
