e84afffea0fb6bb79f8da002b6ae4acdb4ae94e3
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-01-04 19:15:00 -0800

    Motion sensor only outputs to onboard LED.

--- lighting/lighting.ino
+++ lighting/lighting.ino
@@ -1,9 +1,4 @@
-#include <Adafruit_NeoPixel.h>
-
 #define BAUD_RATE 9600
-
-#define DELAY_LOOP_ACTIVE    10
-#define DELAY_LOOP_INACTIVE 500
 
 // Motion Sensor
 // Component: HC-SR501
@@ -11,214 +6,23 @@
 // Digital output
 #define PIN_PIR A2
 
-// Light
-#define PIN_LED 6
-#define FADE_ON_SECONDS 3
-#define NUM_LEDS_ON_IN_ANIMATION 15
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN_LED, NEO_GRBW + NEO_KHZ800);
-static uint32_t color = strip.Color(255,0,0);
-bool motionDetectedPreviously = false;
-bool motionDetected = false;
+#define PIN_LED 13
 
 void setup() {
   pinMode(PIN_PIR, INPUT);
-
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
 
   Serial.begin(BAUD_RATE);
   Serial.println("setup() complete");
 }
 
 void loop() {
-  // Update state from input
-  updateMotionState();
-
-  // Logging
-  printToSerial();
-  output();
-
-  // Loop delay
-  sleep();
-}
-
-void updateMotionState() {
-  motionDetectedPreviously = motionDetected;
-  motionDetected = digitalRead(PIN_PIR);
-}
-
-void printToSerial() {
-  static unsigned long startTime;
-  static unsigned long endTime;
-  static unsigned long duration;
-
-  if (motionDetectedPreviously != motionDetected) {
-    if (motionDetected) {
-      Serial.println("Motion detected! *beep boop*");
-      startTime = millis();
-    } else {
-      Serial.println("Waiting for movement...");
-      endTime = millis();
-      duration = (endTime - startTime) / 1000;
-      Serial.println("Active for " + String(duration, DEC) + " seconds");
-    }
-  }
-}
-
-/**
- * Output state to lights
- */
-void output() {
-  if (motionDetectedPreviously != motionDetected) {
-    if (motionDetected) {
-      outputAnimation();
-      fadeOn();
-    }
-  }
-
-  // normal behavior
-  else {
-    outputLight();
-  }
-}
-
-void fadeOn() {
-  static int wait = FADE_ON_SECONDS * 1000 / 255;
-  static uint32_t color;
-  for (int i=0; i<255; i++) {
-    for (int p=0; p<strip.numPixels(); p++) {
-      strip.setPixelColor(p, strip.Color(i,i,i));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-void outputLight() {
-  static uint32_t color;
-  if (motionDetected) {
-    color = strip.Color(255, 255, 255);
+  static bool pirActive;
+  pirActive = digitalRead(PIN_PIR);
+  Serial.println(pirActive, DEC);
+  if (pirActive) {
+    digitalWrite(PIN_LED, HIGH);
   } else {
-    color = strip.Color(0,0,0);
+    digitalWrite(PIN_LED, LOW);
   }
-  for (int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, color);
-  }
-  strip.show();
+  delay(500);
 }
-
-void outputAnimation() {
-
-  static int wait = 10;
-  static uint32_t white = strip.Color(255, 255, 255);
-  static uint32_t black = strip.Color(0, 0, 0);
-
-  // initialize
-  for (int i=0; i<NUM_LEDS_ON_IN_ANIMATION; i++) {
-    strip.setPixelColor(i, color);
-  }
-  // cycle forward
-  for (int i=NUM_LEDS_ON_IN_ANIMATION-1; i<strip.numPixels(); i++) {
-    iterate();
-    strip.setPixelColor(i, color);
-    for (int j=i; j>=i-NUM_LEDS_ON_IN_ANIMATION; j--) {
-      strip.setPixelColor(j, color);
-    }
-    if (i >= NUM_LEDS_ON_IN_ANIMATION) {
-      strip.setPixelColor(i-NUM_LEDS_ON_IN_ANIMATION, black);
-    }
-    strip.show();
-    delay(wait);
-  }
-  // cycle back
-  for (int i=strip.numPixels()-NUM_LEDS_ON_IN_ANIMATION-1; i>=0; i--) {
-    iterate();
-    strip.setPixelColor(i, color);
-    for (int j=i; j<=i+NUM_LEDS_ON_IN_ANIMATION; j++) {
-      strip.setPixelColor(j, color);
-    }
-    if (i < strip.numPixels()-NUM_LEDS_ON_IN_ANIMATION) {
-      strip.setPixelColor(i+NUM_LEDS_ON_IN_ANIMATION, black);
-    }
-    strip.show();
-    delay(wait);
-  }
-  // cycle forward
-  for (int i=NUM_LEDS_ON_IN_ANIMATION; i<strip.numPixels(); i++) {
-    iterate();
-    strip.setPixelColor(i, color);
-    for (int j=i; j>=i-NUM_LEDS_ON_IN_ANIMATION; j--) {
-      strip.setPixelColor(j, color);
-    }
-    if (i >= NUM_LEDS_ON_IN_ANIMATION) {
-      strip.setPixelColor(i-NUM_LEDS_ON_IN_ANIMATION, black);
-    }
-    strip.show();
-    delay(wait);
-  }
-  // cycle back
-  for (int i=strip.numPixels()-NUM_LEDS_ON_IN_ANIMATION-1; i>=0; i--) {
-    iterate();
-    strip.setPixelColor(i, color);
-    for (int j=i; j<=i+NUM_LEDS_ON_IN_ANIMATION; j++) {
-      strip.setPixelColor(j, color);
-    }
-    if (i < strip.numPixels()-NUM_LEDS_ON_IN_ANIMATION) {
-      strip.setPixelColor(i+NUM_LEDS_ON_IN_ANIMATION, black);
-    }
-    strip.show();
-    delay(wait);
-  }
-  delay(50);
-
-  // turn off
-  for (int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, black);
-  }
-  strip.show();
-}
-
-void iterate() {
-  static int iterationsInAnimation = (strip.numPixels()-NUM_LEDS_ON_IN_ANIMATION)*4;
-  static int count = 0;
-
-  static int red = 255;
-  static int green = 0;
-  static int blue = 0;
-  static int colorRange;
-  colorRange = map(count, 0, iterationsInAnimation, 0, 1530);
-
-  if (colorRange>=0 && colorRange<=255) {
-    green = colorRange;
-  } else if (colorRange>=256 && colorRange<=511) {
-    red = 511-colorRange;
-  } else if (colorRange>=512 && colorRange<=767) {
-    blue = colorRange-512;
-  } else if (colorRange>=768 && colorRange<=1023) {
-    green = 1023-colorRange;
-  } else if (colorRange>=1024 && colorRange<=1279) {
-    red = colorRange-1024;
-  } else if (colorRange>=1280 && colorRange<=1530) {
-    blue = 1530-colorRange;
-  }
-
-  color = strip.Color(red, green, blue);
-
-  if (count == iterationsInAnimation) {
-    count = 0;
-  } else {
-    count = count + 1;
-  }
-}
-
-/**
- * Delay application depending on state.
- */
-void sleep() {
-  if (motionDetected) {
-    delay(DELAY_LOOP_ACTIVE);
-  } else {
-    delay(DELAY_LOOP_INACTIVE);
-  }
-}
-
